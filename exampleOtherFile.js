336016ab68b7bfd3e05ce5e57c5e2baff11d8f85
Frosty <frosticles922@gmail.com>
2024-04-06 08:52:08 +0800

    Added Json File

--- exampleOtherFile.js
+++ exampleOtherFile.js
@@ -1,5 +1,5 @@
-// import syntax works for React, needs different tweals for vanilla JS
-// import {logTheTeam as originalLogger} from "./script";
+// import syntax works for React, needs different tweaks for vanilla JS
+// import {logTheTeam as originalLogger}  from "./script";
 
 
 
@@ -8,4 +8,41 @@ logTheTeam();
 
 
 console.log(person.name);
+
+// import data from "./jsonExample.json";
+
+// let data = fetch("./jsonExample.json").then(data => {return data});
+
+// console.log(data);
+
+let exampleJson = {
+	"name":"Alex",
+	"favouriteFood":[
+		"pizza",
+		"pies",
+		"mangoes"
+	]
+}
+
+
+/*
+
+1. JSON.parse to convert a JSON string into an object
+2. Modify the object however you want
+3. Save the JSON as a JSON string by stringifying it
+
+*/
+
+let jsonString = JSON.stringify(exampleJson);
+
+console.log(JSON.stringify(exampleJson));
+
+// Load your JSON 
+let processedJson = JSON.parse(jsonString);
+
+console.log(processedJson);
+processedJson.name = "Batman";
+
+console.log(JSON.stringify(processedJson));
+
 
