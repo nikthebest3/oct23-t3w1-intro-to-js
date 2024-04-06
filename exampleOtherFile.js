0d79046f6a229f3cb09ad87e9ec73713b602177e
Frosty <frosticles922@gmail.com>
2024-04-06 08:43:01 +0800

    Added Another JS File

--- exampleOtherFile.js
+++ exampleOtherFile.js
@@ -1,3 +1,10 @@
+// import syntax works for React, needs different tweals for vanilla JS
+// import {logTheTeam as originalLogger} from "./script";
+
+
+
+logTheTeam();
+// originalLogger();
 
 
 console.log(person.name);
